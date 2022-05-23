f3b89f99a2a673607c94551ef3573748f65a0179
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-23 15:37:32 +0200

    struttura

---
+++ resources/views/partial/_header.blade.php
@@ -0,0 +1,34 @@
+<header>
+    <div class='cont-top-blu'>
+        <div class='cont-in-blu'>
+            <div class='txt-sx'>
+                <h2>
+                    dc power visa
+                </h2>
+            </div>
+            <div class='txt-dx'>
+                <h2>
+                    additional dc sites
+                </h2>
+            </div>
+        </div>
+        
+    </div>
+    <div class='cot-bottom'>
+        <img src="" alt="">
+        <ul class='menù'>
+            <li>characters</li>
+            <li>comics</li>
+            <li>movies</li>
+            <li>tv</li>
+            <li>games</li>
+            <li>collectibles</li>
+            <li>videos</li>
+            <li>fans</li>
+            <li>news</li>
+            <li>shop</li>
+
+        </ul>
+        <input type="text" placeholder="Search">
+    </div>
+</header>
