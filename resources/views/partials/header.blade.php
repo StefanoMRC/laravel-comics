6ae83ec5b72779eaf0e0b3cf29586cb5001ee8d3
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-23 16:15:46 +0200

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,34 @@
+<header>
+    <div class='cont-top-blu'>
+        <div class='cont-in-blu'>
+            <div class='txt-sx'>
+                <h2>
+                    dc power visa
+                </h2>
+            </div>
+            <div class='txt-dx'>
+                <h2>
+                    additional dc sites
+                </h2>
+            </div>
+        </div>
+        
+    </div>
+    <div class='cot-bottom'>
+        <img src="{{asset('images/dc-logo.png')}}" alt="">
+        <ul class='menu'>
+            <li><a href="">characters</a> </li>
+            <li><a href="">comics</a> </li>
+            <li><a href="">movies</a> </li>
+            <li><a href="">tv</a> </li>
+            <li><a href="">games</a> </li>
+            <li><a href="">collectibles</a> </li>
+            <li><a href="">videos</a> </li>
+            <li><a href="">fans</a> </li>
+            <li><a href="">news</a> </li>
+            <li><a href="">shop</a> </li>
+
+        </ul>
+        <input type="text" placeholder="Search">
+    </div>
+</header>
