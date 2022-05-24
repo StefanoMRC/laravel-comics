cff4b06c874a731e9cf4f734967c3722ceaac823
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-24 17:04:39 +0200

    80% footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,120 @@
+<div class="cont_footer_principale p-10">
+    <div class="w-80 d-flex p-10">
+        <div>
+            <div class="d-flex">
+                <div>
+                    <div class="m-10-x">
+                        <h2 class="uppercase color_white">dc comics</h2>
+                        <ul>
+                            <li class="p-y-5">
+                                <a href="">Characters</a>
+                            </li>
+                            <li class="p-y-5">
+                                <a href="">Comics</a>
+                                
+                            </li>
+                            <li class="p-y-5">
+                                <a href="">Movies</a>
+                                
+                            </li>
+                            <li class="p-y-5">
+                                <a href="">TV</a>
+                            </li>
+                            <li class="p-y-5">
+                                <a href="">Games</a>
+                            </li>
+                            <li class="p-y-5">
+                                <a href="">Videos</a>
+                            </li>
+                            <li class="p-y-5">
+                                <a href="">News</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="m-10-x">
+                        <h2 class="uppercase color_white">shop</h2>
+                        <ul>
+                            <li class="p-y-5">
+                                <a href="">Shop DC</a>
+                            </li>
+                            <li class="p-y-5">
+                                <a href="">Shop DC Collectibles</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+            
+            
+                <div class="m-10-x">
+                    <h2 class="uppercase color_white">dc</h2>
+                    <ul>
+                        <li class="p-y-5">
+                            <a href="">Term Of Use</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Privacy policy (New)</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Ad Choices</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Advestising</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Jobs</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Subscription</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Talent Workshop</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">CPSC Certificate</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Ratings</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Shop Help</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="m-10-x">
+                    <h2 class="uppercase color_white">sites</h2>
+                    <ul>
+                        <li class="p-y-5">
+                            <a href="">DC</a>
+                        </li>
+                        <li>
+                            <a href="">MAD Magazine</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">DC Kids</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">DC Universe</a>
+                        </li>
+                        <li class="p-y-5">
+                            <a href="">DC Power Visa</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+        <div class="cont-logo-big">
+            <div class="logo-big">
+                <img src="{{asset('images/dc-logo-bg.png')}}" alt="">
+            </div>
+        </div>
+        
+    </div>
+</div>
+<div>
+
+</div>
